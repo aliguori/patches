539a792601af665018bc39a0ca310f9084770dc2
Anthony Liguori <aliguori@us.ibm.com>
2013-04-08 13:08:22 -0500

    apply: add Message-id and SoB to merged pull requests
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- patchlib/apply.py
+++ patchlib/apply.py
@@ -13,7 +13,7 @@
 import config, mbox, gitcmd, data
 from list import find_subseries
 from series import *
-from subprocess import call
+from subprocess import call, check_output
 from util import call_teed_output
 import os
 
@@ -28,13 +28,19 @@ def apply_patch(pathname, **kwds):
     opts.append(pathname)
     return call_teed_output(['git', 'am'] + opts, **kwds)
 
-def apply_pull_request(pull_request, **kwds):
+def apply_pull_request(msg, **kwds):
+    pull_request = msg['pull-request']
     uri = pull_request['uri'].encode('ascii', errors='ignore')
     refspec = pull_request['refspec'].encode('ascii', errors='ignore')
     remotes = gitcmd.get_remotes(**kwds)
+    sob = None
 
     if 'signed-off-by' in kwds:
         del kwds['signed-off-by']
+        name = check_output(['git', 'config', 'user.name'], **kwds).strip()
+        email = check_output(['git', 'config', 'user.email'], **kwds).strip()
+        sob = '%s <%s>' % (name, email)
+
     if 'interactive' in kwds:
         del kwds['interactive']
 
@@ -47,11 +53,25 @@ def apply_pull_request(pull_request, **kwds):
     if s != 0:
         return s, o
 
-    return call_teed_output(['git', 'merge', '%s/%s' % (remote, refspec)], **kwds)
+    s, o = call_teed_output(['git', 'merge', '--no-ff', '%s/%s' % (remote, refspec)], **kwds)
+    if s != 0:
+        return s, o
+
+    # I am not proud of this but I don't see any other option
+    if o.strip() == 'Already up-to-date.':
+        return 0, o
+
+    o = check_output(['git', 'log', '-1', '--format=%B', 'HEAD'], **kwds)
+    o += '\n'
+    o += 'Message-id: %s\n' % msg['message-id']
+    if sob:
+        o += 'Signed-off-by: %s\n' % sob
+
+    return call_teed_output(['git', 'commit', '--amend', '-m', o], **kwds)
 
 def apply_series(series, **kwds):
    if is_pull_request(series):
-       return apply_pull_request(series['messages'][0]['pull-request'], **kwds)
+       return apply_pull_request(series['messages'][0], **kwds)
    elif is_broken(series):
        raise Exception('Cannot apply series: series is either incomplete or improperly threaded.')
    elif not 'mbox_path' in series:
